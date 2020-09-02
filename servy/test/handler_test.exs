985c0fb3d47725b8648a242d83d3dd9329421c70
Herminio Torres <herminiocesar@gmail.com>
2020-09-02 07:36:15 -0300

    Function Clauses

--- servy/test/handler_test.exs
+++ servy/test/handler_test.exs
@@ -3,23 +3,35 @@ defmodule HandlerTest do
 
   alias Servy.Handler
 
-  test "send request and receive the response" do
-    request = """
-    GET /wildthings HTTP/1.1
+  test "send a GET request to /wildthings without params and receive the response" do
+    request = build_request(%{ method: "GET", path: "/wildthings" })
+
+    assert Handler.handle(request) == expected_response("Bears, Lions, Tigers")
+  end
+
+  test "send a GET request to /bears without params and receive the response" do
+    request = build_request(%{ method: "GET", path: "/bears" })
+
+    assert Handler.handle(request) == expected_response("Teddy, Smokey, Paddington")
+  end
+
+  defp build_request(%{ method: method, path: path }) do
+    """
+    #{method} #{path} HTTP/1.1
     Host: example.com
     User-Agent: ExampleBrowser/1.0
     Accept: */*
 
     """
+  end
 
-    expected_response = """
+  defp expected_response(body) do
+    """
     HTTP/1.1 200 OK
     Content-Type: text/html
-    Content-Length: 20
+    Content-Length: #{String.length(body)}
 
-    Bears, Lions, Tigers
+    #{body}
     """
-
-    assert Handler.handle(request) == expected_response
   end
 end

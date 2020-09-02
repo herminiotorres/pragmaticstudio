f13486cd7c7dcf5832c4aeb5848420f20f688655
Herminio Torres <herminiocesar@gmail.com>
2020-09-02 08:06:59 -0300

    Advanced Pattern Matching

--- servy/test/handler_test.exs
+++ servy/test/handler_test.exs
@@ -3,21 +3,49 @@ defmodule HandlerTest do
 
   alias Servy.Handler
 
-  test "send a GET request to /wildthings without params and receive the response" do
+  test "send a GET request to /wildthings and receive a response with status 200" do
     request = build_request(%{ method: "GET", path: "/wildthings" })
 
-    assert Handler.handle(request) == expected_response("Bears, Lions, Tigers")
+    expected_response = build_response(%{status: 200, body: "Bears, Lions, Tigers"})
+
+    assert Handler.handle(request) == expected_response
   end
 
-  test "send a GET request to /bears without params and receive the response" do
-    request = build_request(%{ method: "GET", path: "/bears" })
+  test "send a GET request to /bears and receive a response with status 200" do
+    request = build_request(%{method: "GET", path: "/bears"})
 
-    assert Handler.handle(request) == expected_response("Teddy, Smokey, Paddington")
+    expected_response = build_response(%{status: 200, body: "Teddy, Smokey, Paddington"})
+
+    assert Handler.handle(request) == expected_response
   end
 
-  defp build_request(%{ method: method, path: path }) do
+  test "send a GET request to /bears/1 and receive a response with status 200" do
+    request = build_request(%{method: "GET", path: "/bears/1"})
+
+    expected_response = build_response(%{status: 200, body: "Bear 1"})
+
+    assert Handler.handle(request) == expected_response
+  end
+
+  test "send a DELETE request to /bears/1 and receive a response with status 403" do
+    request = build_request(%{method: "DELETE", path: "/bears/1"})
+
+    expected_response = build_response(%{status: 403, body: "Deleting a bear is forbidden!"})
+
+    assert Handler.handle(request) == expected_response
+  end
+
+  test "send a GET request to /bigfoot and receive a response with status 404" do
+    request = build_request(%{method: "GET", path: "/bigfoot"})
+
+    expected_response = build_response(%{status: 404, body: "No /bigfoot here!"})
+
+    assert Handler.handle(request) == expected_response
+  end
+
+  defp build_request(data) do
     """
-    #{method} #{path} HTTP/1.1
+    #{data.method} #{data.path} HTTP/1.1
     Host: example.com
     User-Agent: ExampleBrowser/1.0
     Accept: */*
@@ -25,13 +53,24 @@ defmodule HandlerTest do
     """
   end
 
-  defp expected_response(body) do
+  defp build_response(data) do
     """
-    HTTP/1.1 200 OK
+    HTTP/1.1 #{data.status} #{status_reason(data.status)}
     Content-Type: text/html
-    Content-Length: #{String.length(body)}
+    Content-Length: #{String.length(data.body)}
 
-    #{body}
+    #{data.body}
     """
+  end
+
+  defp status_reason(code) do
+    %{
+      200 => "OK",
+      201 => "Create",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error",
+    }[code]
   end
 end

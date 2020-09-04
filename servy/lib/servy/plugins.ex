f8d497a672a0e5d0bf85fc3964c0cefd7ffd99e1
Herminio Torres <herminiocesar@gmail.com>
2020-09-04 10:18:22 -0300

    Modeling With Structs

--- servy/lib/servy/plugins.ex
+++ servy/lib/servy/plugins.ex
@@ -1,21 +1,23 @@
 defmodule Servy.Plugins do
+  alias Servy.Conv
+
   require Logger
 
   @doc """
     Logs 404 requests.
   """
-  def track(%{status: 404, path: path} = conv) do
+  def track(%Conv{status: 404, path: path} = conv) do
     Logger.warn("Warning: #{path} is on the loose!")
     conv
   end
-  def track(conv), do: conv
+  def track(%Conv{} = conv), do: conv
 
-  def log(conv), do: IO.inspect(conv)
+  def log(%Conv{} = conv), do: IO.inspect(conv)
 
-  def rewrite_path(%{path: "/wildlife"} = conv) do
+  def rewrite_path(%Conv{path: "/wildlife"} = conv) do
     %{conv | path: "/wildthings" }
   end
-  def rewrite_path(%{path: path} = conv) do
+  def rewrite_path(%Conv{path: path} = conv) do
     regex = ~r{\/(?<thing>\w+)\?id=(?<id>\d+)}
     captures = Regex.named_captures(regex, path)
     rewrite_path_captures(conv, captures)
